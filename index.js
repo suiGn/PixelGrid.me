f1c50e3df47d9dc78d59785ec0a66baca5eb1b29
Sui Gn <natsynapses@gmail.com>
2024-02-27 17:47:43 -0500

    Clean Start and Structure.
    3.4.8 Start using ECMAScript Module (ECMSA) approach and its modular approach for i.mlearning all.this with neurons.me

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Indexing PixelGrid.js;");
+/* In this.pixelgrid/index.js
+ⓝⓔⓤⓡⓞⓝⓢ.ⓜⓔ
+🆂🆄🅸🅶🅽 */
+
+/**
+ * @module pixelgrid
+ * @description Exports the main functionalities of the this.pixelgrid library.
+ */
+
+import PixelGrid from './src/PixelGrid.js';
+
+/**
+ * The main export object of this.pixelgrid package.
+ * @type {Object}
+ */
+const pixelgrid = {
+  PixelGrid
+};
+
+export default pixelgrid;
+
+console.log("PixelGrid loaded;");
